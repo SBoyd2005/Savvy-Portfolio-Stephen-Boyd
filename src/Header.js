f84def7c83096b44c245de8dfa676986ef7da85a
Stephen Boyd <Stephen.Boyd48@gmail.com>
2019-11-28 21:15:06 -0600

    CSS Color Changes and Content Changes

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,9 @@ export default function Header(){
 <div id="header">
         <div class="container">
             <h1>Stephen P. Boyd</h1>
+            <div id = "img">
+            <img src= "./Boyd.jpg">
+            </div>
             <h2>Quality Assurance Software Engineer, Saint Louis, Missouri</h2>
         </div>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,5 @@
 export default function Home(){
     return `
-    <div id = "img">
-    <img src= "Boyd.jpg">
-    </div>
-    <br>
-    <figure>Land's End, San Francisco, CA</figure>
-    <i>“Take positive care of your mind, and it would surely take positive care of your life.”
-        ― Edmond Mbiaka</i>
     <h3>
         <p>I am a Quality Assurance Software Engineer, based St. Louis, Missouri. I have obtained an extensive academic background, along with a diverse
             professional background. I strive to constantly improve each day mind, body, and soul everyday. </p>
