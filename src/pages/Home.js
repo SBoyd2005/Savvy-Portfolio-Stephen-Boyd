70efff328ae87d9665e486a0984094f5a4dd6103
Stephen Boyd <Stephen.Boyd48@gmail.com>
2019-11-29 19:41:25 -0600

    Added CSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 export default function Home(){
     return `
     <div id = "img">
-        <img src="url(./Documents/Boyd.jpg)"
+        <img src="Boyd.jpg"
             alt="Stephen Boyd" class="center" />
         </div>
     <h3>
-        <p>I am a Quality Assurance Software Engineer, based St. Louis, Missouri. I have obtained an extensive academic background, along with a diverse
+        <p>I am a Software Engineer, based St. Louis, Missouri. I have obtained an extensive academic background, along with a diverse
             professional background. I strive to constantly improve each day mind, body, and soul everyday. </p>
     </h3>
     `;
