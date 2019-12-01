be387f9d1269db59006432edb2edecfe376bf6d5
Stephen Boyd <Stephen.Boyd48@gmail.com>
2019-11-30 21:33:18 -0600

    Changed Containers

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,10 @@
 export default function About(){
     return `
             <div id = "img">
+            <div class="container">
             <img src="https://www.facebook.com/photo.php?fbid=10156150264192134&set=pob.646542133&type=3"
                 alt="Kelly Chase and myself with the Stanley Cup, Saint Louis, Missouri" class="center" >
+            </div>
             </div>
         <br>
         <p>
@@ -12,9 +14,12 @@ export default function About(){
         </p>
         <br>
         <div id = "img">
+        <div class="container">
         <img src="https://www.facebook.com/photo.php?fbid=10155346884702134&set=pb.646542133.-2207520000.0.&type=3&theater"
             alt="Napa Valley on a Beautiful Day, Sonoma, California" class="center" >
         </div>
+        </div>
+        <br>
         <p>
             In my spare time, I enjoy exercising, particulary running and weight lifting. I love to challenge
             myself and want to do a half-marathon and full marathon soon! In addition, I am a world-traveler
@@ -24,8 +29,10 @@ export default function About(){
             Brooks, the Beach Boys, Journey, and Def Leppard in concert.
         </p>
         <div id = "img">
+        <div class="container">
         <img src="https://www.facebook.com/photo.php?fbid=10155073950537134&set=pb.646542133.-2207520000.0.&type=3&theater"
             alt="Running at the Park on a Beautiful Day" class="center" >
+        </div>
         </div>
 `;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 export default function Home(){
     return `
     <div id = "img">
-        <img src="Boyd.jpg"
-            alt="Stephen Boyd" class="center">
-        </div>
+    <div class="container">
+        <img src="Boyd.jpg" alt="Stephen Boyd" class="center">
+    </div>
+    </div>
+    <br>
     <h3>
         <p>I am a Software Engineer, based St. Louis, Missouri. I have obtained an extensive academic background, along with a diverse
             professional background. I strive to constantly improve each day mind, body, and soul everyday. </p>
